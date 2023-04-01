96b95e7796d2a080fb4d582447298932f0c549b6
lonewanderer27 <adrianejames27@gmail.com>
2023-04-01 12:42:57 +0800

    add comments for misc & test calculators

--- src/calculators/misc.ts
+++ src/calculators/misc.ts
@@ -1,5 +1,14 @@
-import { evaluate, parser } from "mathjs";
+import { parser } from "mathjs";
 
+/**
+ * Formats a given function string to include the variable letter specified as a parameter.
+ * If the string already includes "f(x)", "f(Xn)", or "f'(Xn)", it is returned as is.
+ * Otherwise, the function string is formatted as "f(variableLetter) = functionString".
+ * 
+ * @param funcString - The original function string to format.
+ * @param letterVar - The variable letter to include in the formatted function string.
+ * @returns The formatted function string.
+ */
 export function formatFunc(funcString: string, letterVar: string){
   let formattedFunction = ""
   if (funcString.includes("f(x)") || funcString.includes("f(Xn)") || funcString.includes("f'(Xn)")) {
@@ -13,6 +22,13 @@ export function formatFunc(funcString: string, letterVar: string){
   return formattedFunction;
 }
 
+/**
+ * Tests if a given function string is valid by evaluating it with a sample number.
+ * 
+ * @param funcString - The function string to test.
+ * @param sampleNum - A sample number to use for evaluating the function string.
+ * @returns `true` if the function string is valid, `false` otherwise.
+ */
 export function testFunc(funcString: string, sampleNum: number = 1) {
   const p = parser();
   try {
--- src/calculators/test.ts
+++ src/calculators/test.ts
@@ -1,19 +1,26 @@
 import { rowType, rowsType } from "../types";
 
+// This function generates an array of random values for a given number of rows
 export function randomTableVal(rows: number): rowsType {
+
+  // Create an empty array to store the rows
   let myArray: rowType[] = [];
 
+  // Loop through the desired number of rows
   for(let i = 0; i < rows; i++) {
+
+    // Add a new row to the array with random values for each column
     myArray.push({
-      n: i + 1,
-      a: i * 2,
-      b: i * 3,
-      c: i * 4,
-      d: Math.sqrt(i * 4),
-      e: Math.abs((i * 2) - (i * 3)),
-      less_than_error: (i < 5)
+      n: i + 1,                      // incrementing integer value starting from 1
+      a: i * 2,                      // column "a" gets a value equal to 2 times the row index
+      b: i * 3,                      // column "b" gets a value equal to 3 times the row index
+      c: i * 4,                      // column "c" gets a value equal to 4 times the row index
+      d: Math.sqrt(i * 4),           // column "d" gets the square root of 4 times the row index
+      e: Math.abs((i * 2) - (i * 3)),// column "e" gets the absolute difference between 2 times the row index and 3 times the row index
+      less_than_error: (i < 5)       // column "less_than_error" gets a boolean value indicating whether the row index is less than 5
     });
   }
 
+  // Return the completed array
   return myArray;
 }
