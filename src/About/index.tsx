73a751cccd0965853dd8432e4b23e371953a9386
lonewanderer27 <adrianejames27@gmail.com>
2023-04-01 15:04:39 +0800

    add program description in about

--- src/About/index.tsx
+++ src/About/index.tsx
@@ -10,8 +10,19 @@ export default function About(){
     > 
       <Toolbar/>
       <Typography paragraph>
-        Made by MarkTheBurst and lonewanderer27!<br></br>
-        Yet to be filled X0X0
+      Rootify is a numerical methods tool built using React. It allows users to calculate the roots of a function using two different methods: the bisection method and the Newton-Raphson method.
+      </Typography>
+
+      <Typography paragraph>
+      Users can enter a mathematical function, specify the method they want to use, and set parameters such as the initial interval and the maximum number of iterations. The website will then generate a table showing the calculated values for each iteration of the selected method, as well as whether or not the error is less than a specified tolerance.
+      </Typography>
+
+      <Typography paragraph>
+      The user can then analyze the table to determine the root of the function to the desired degree of accuracy. The website uses the MathJS library to parse and evaluate the user's mathematical function.
+      </Typography>
+
+      <Typography variant="h5">
+        Who developed this awesome app?
       </Typography>
     </Box>
   )
